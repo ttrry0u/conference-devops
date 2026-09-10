5be986e76acb1778aef822ab52dca2b6a37f1755
ttrry0u <andrejslivcenko268@gmail.com>
2026-09-10 22:20:04 +0300

    fix: закомментированы неиспользуемые импорты

--- app/routers/science.py
+++ app/routers/science.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, #HTTPException
+from fastapi import APIRouter, Depends #HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
 from app.models.science import Invitation, Abstract
