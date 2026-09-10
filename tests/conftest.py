1fd8cf21bb08db5339975d588550ab40d597fbe9
Maria <xydr0xidd@gmail.com>
2026-09-10 16:29:21 +0300

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,7 @@ TEST_DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(TEST_DATABASE_URL, connect_args={"check_same_thread": False})
 TestingSessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
+
 @pytest.fixture(autouse=True)
 def setup_db():
     Base.metadata.drop_all(bind=engine)
@@ -16,11 +17,13 @@ def setup_db():
     yield
     Base.metadata.drop_all(bind=engine)
 
+
 def override_get_db():
     db = TestingSessionLocal()
     try:
         yield db
     finally:
         db.close()
+
 
 app.dependency_overrides[get_db] = override_get_db
